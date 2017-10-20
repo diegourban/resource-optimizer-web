04af16c6f5049ecc37e101854da9d811f11facfc
Diego Leonardo Urban <urban@dynamix.com.br>
2017-10-20 09:54:07 -0200

    handling invalid content type

--- public/js/upload.js
+++ public/js/upload.js
@@ -27,12 +27,21 @@ $("#input-upload").on("change", function(){
       processData: false,
       contentType: false,
       success: function(data){
+        $(".btn-upload").prop('disabled', false);
         console.log("Upload feito com sucesso!\n" + data);
         var result = JSON.parse(data);
         console.log(result);
         exibirAreaDownload(result);
       },
+      error: function(xhr) {
+        $(".btn-upload").prop('disabled', false);
+        console.log(xhr.status);
+        console.log(xhr.statusText);
+        console.log(xhr.responseText);
+        exibirMensagemErro(xhr);
+      },
       xhr: function() {
+        $(".btn-upload").prop('disabled', true);
         var xhr = new XMLHttpRequest();
 
         xhr.upload.addEventListener("progress", function(evt) {
@@ -61,6 +70,7 @@ $("#input-upload").on("change", function(){
 function esconderAreaDownload() {
   $("#area-download").hide(1000);
   $("#progress-bar-upload").addClass("progress-bar-striped active");
+  $("#progress-bar-upload").removeClass("progress-bar-danger progress-bar-success");
 }
 
 function exibirAreaDownload(result) {
@@ -68,7 +78,14 @@ function exibirAreaDownload(result) {
   $("#span-espaco").text((result.stats.difference / 1024).toFixed(2)  + " KB a menos");
   $("#span-percentagem").text("Economia de " + (100 - (100 * result.stats.ratio)).toFixed(2) + "%");
   $("#progress-bar-upload").removeClass("progress-bar-striped active");
+  $("#progress-bar-upload").addClass("progress-bar-success");
   $("#progress-bar-upload").html("Finalizado");
   $("#area-download .btn-download").attr("href", "http://localhost:3001" + result.url);
   $("#area-download").show(1000);
+}
+
+function exibirMensagemErro(xhr) {
+  $("#progress-bar-upload").removeClass("progress-bar-striped active");
+  $("#progress-bar-upload").addClass("progress-bar-danger");
+  $("#progress-bar-upload").html(xhr.responseText);
 }
