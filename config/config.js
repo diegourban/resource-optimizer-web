a1a0d6d9aaaf045fae5e3143374f490c0286d09e
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-10-21 10:35:53 -0200

    conigs and logs

--- config/config.js
+++ config/config.js
@@ -4,3 +4,10 @@ config.express = {
   port: process.env.EXPRESS_PORT || 3001,
   ip: "localhost"
 }
+
+config.app = {
+  acceptedMIMEType: ["application/zip", "application/x-zip-compressed"],
+  uploadsFolder: "/uploads",
+  extractedFolder: "__extracted",
+  optimizedFolder: "__optimized"
+}
