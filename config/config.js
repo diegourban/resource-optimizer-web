63bb363bf6dd07ab1339253535dd605f8d317519
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-10-21 16:49:49 -0200

    refactorzord

--- config/config.js
+++ config/config.js
@@ -6,8 +6,15 @@ config.express = {
 }
 
 config.app = {
+  uploadName: "projectFile",
   acceptedMIMEType: ["application/zip", "application/x-zip-compressed"],
   uploadsFolder: "/uploads",
   extractedFolder: "__extracted",
   optimizedFolder: "__optimized"
+}
+
+config.api = {
+  ip: "localhost",
+  port: "3000",
+  url: "/api/minify"
 }
